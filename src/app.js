c0fd42e91c6edfa33570af6fd51273fb677d1c71
DaniPinelli <elidaniypri@gmail.com>
2021-03-03 12:31:30 -0300

    Configuraciones iniciales y creacion de modelo de tareas

--- src/app.js
+++ src/app.js
@@ -1,4 +1,32 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => console.log("Server on port 3000"));
+//conecting to db
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('DB conected'))
+    .catch(err => console.log(err));
+
+//importing routes
+const indexRoutes = require('./routes/index');
+
+//settings
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({
+    extended: false
+}));
+
+//routes 
+app.use('/', indexRoutes);
+
+//starting the server
+
+app.listen(3000, () => console.log(`Server on port ${app.get("port")}`));
